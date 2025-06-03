4d7f26c8fc8b92889bed81914242c44b738f0190
bedoom <3163026087@qq.com>
2025-06-03 16:12:04 +0000

    Add initial Qedis configuration file with comprehensive settings
    - Configured daemonization, port, and binding options.
    - Set logging level and log file output.
    - Defined database settings and snapshotting behavior.
    - Configured replication options including slave settings.
    - Established security measures with authentication and command renaming.
    - Specified memory limits and eviction policies.
    - Included append-only file settings for enhanced durability.
    - Configured Lua scripting and slow log parameters.
    - Added advanced configuration options for background tasks and backend settings.
    - Enabled cluster configuration with center and set ID definitions.

---
+++ QBase/include/ConfigParser.hpp
@@ -0,0 +1,77 @@
+#pragma once
+#include <map>
+#include <string>
+#include <vector>
+#include <sstream>
+
+#ifdef CONFIG_DEBUG
+#include <iostream>
+#endif
+
+class ConfigParser
+{
+public:
+    bool Load(const char* FileName);
+
+    template<typename T>
+    T GetData(const char* key, const T* default_ = T()) const;
+
+    const std::vector<std::string>& GetDataVector(const char* key) const;
+
+#ifdef CONFIG_DEBUG
+    void Print()
+    {
+        std::cout << "//////////////////"<< std::endl;
+        std::map<std::string, std::vector<std::string> >::const_iterator it = data_.begin();
+        while (it != data_.end())
+        {
+            std::cout << it->first << ": ";
+            for (const auto& val : it->second)
+            {
+                std::cout << val << " ";
+            }
+            std::cout << "\n";
+            ++it;
+        }
+    }
+#endif
+
+private:
+    typedef std::map<std::string, std::vector<std::string> > Data;
+
+    Data data_;
+    
+    template<typename T>
+    T _ToType(const std::string& data) const;
+};
+
+template<typename T>
+inline  T  ConfigParser::_ToType(const std::string& data) const
+{
+    T t;
+    std::istringstream os(data);
+    os >> t;
+    return t;
+}
+
+template<>
+inline const char* ConfigParser::_ToType(const std::string& data) const
+{
+    return data.c_str();
+}
+
+template<>
+inline std::string ConfigParser::_ToType(const std::string& data) const
+{
+    return data;
+}
+
+template<typename T>
+T ConfigParser::GetData(const char* key, const T* default_) const
+{
+    auto it = data_.find(key);
+    if(it == data_.end())
+        return default_;
+    
+    return _ToType<T>(it->second[0]); // only return first value
+}
